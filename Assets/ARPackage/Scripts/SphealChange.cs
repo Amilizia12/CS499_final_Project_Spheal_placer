f3e06e71e9a3247c526662429a746b412ddbdfe2
amilizia12 <113862554+Amilizia12@users.noreply.github.com>
2024-05-02 15:51:24 -0700

    finsihed product

--- Assets/ARPackage/Scripts/SphealChange.cs
+++ Assets/ARPackage/Scripts/SphealChange.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SphealChange : MonoBehaviour
 {
   PlaceObjectOnPlane manager;
+    int spinCount = 4;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +18,25 @@ public class SphealChange : MonoBehaviour
         
     }
    public void rotate() {
-      manager.spawnedObject  = null; 
+      manager.spawnedObject.transform.rotation *= Quaternion.AngleAxis(90, Vector3.up);
+
     }
    public void shrink() {
       manager.spawnedObject.transform.localScale *= 0.5f;
-
+     }
+    public void spin() {
+          spinCount = 4;
+          Invoke("RoundRotate",1f);
 
     }
+
+   void RoundRotate() {
+     spinCount--;
+     if(spinCount >= 0 ) {
+        rotate();
+        Invoke("RoundRotate",0.4f);
+     }
+   }
    public void grow() {
       manager.spawnedObject.transform.localScale *= 2f;
 
